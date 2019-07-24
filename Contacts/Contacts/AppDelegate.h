971d42e8ef7e6cc2a246d4d6d4bf9b1e1d5f6fab
dllewisjolley <dlewisjolley@gmail.com>
2019-07-24 18:11:31 -0400

    initial commit

---
+++ Contacts/Contacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts
+//
+//  Created by Diante Lewis-Jolley on 7/24/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
